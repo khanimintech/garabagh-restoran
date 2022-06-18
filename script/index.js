83e9837e2d8095dd25a339bc8e51702a099655a1
khanim Tech <khanimintech@gmail.com>
2022-06-18 11:02:29 +0400

    restourant project created

---
+++ script/index.js
@@ -0,0 +1,27 @@
+const header = document.querySelector('.header__back')
+const headerBig = document.querySelector('.header')
+const headmenu = document.querySelectorAll('.head_menu li a')
+
+
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 1) {
+        header.style.backgroundColor = '#fff'
+        header.style.opacity = '1'
+        headerBig.style.boxShadow = '0 0 20px 1px #000'
+        headmenu.forEach((item) => {
+            item.style.color = '#000'
+        })
+    }
+    else {
+        header.style.backgroundColor = 'rgb(145, 145, 145)'
+        header.style.opacity = '0.3'
+        headerBig.style.boxShadow = ''
+        headmenu.forEach((item) => {
+            item.style.color = '#fff'
+        })
+    }
+
+})
+
+
